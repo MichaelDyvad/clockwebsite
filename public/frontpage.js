1d70f98205199a9feeca6b985c7ea133aed5ba38
MichaelDyvad <michaeldyvad@gmail.com>
2022-09-22 12:55:54 +0200

    front folder removed

--- public/frontpage.js
+++ public/frontpage.js
@@ -10,3 +10,10 @@ fetch("/gettime")
     })
 })    
 
+type="text/javascript"
+const months = ['January','February','March','April','May','June','July',
+'August','September','October','November','December'];       
+let tomorrow = new Date();
+tomorrow.setTime(tomorrow.getTime() + (1000*3600*24));       
+document.getElementById("spanDate").innerHTML = months[tomorrow.getMonth()] + " " + tomorrow.getDate()+ ", " + tomorrow.getFullYear(), + " " + tomorrow.getTime();
+ 
