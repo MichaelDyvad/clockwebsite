86ff5293c4d0b8c1e61046cc548819bcd16c5732
MichaelDyvad <michaeldyvad@gmail.com>
2022-09-22 13:16:10 +0200

    added deploy settings

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { get } from "http";
+
 import path from "path";
 const app = express();
 
+
+
+
 app.use(express.static("public"));
+
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("./public/frontpage.html"));
