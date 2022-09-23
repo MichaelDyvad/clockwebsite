b3d115a75b03c65ca1146f9683d02bbfb4b46e02
MichaelDyvad <michaeldyvad@gmail.com>
2022-09-23 13:08:42 +0200

    countdown to summer created and styled

--- public/frontpage.js
+++ public/frontpage.js
@@ -8,12 +8,21 @@ fetch("/gettime")
     document.getElementById("get-time-newyork").addEventListener("click", ()=> {    
         document.getElementById("p-tag-newyork-time").innerText = "current time: " + data.timeNewYork + "\n " + "todays date: " + data.dateNewYork
     })
-})    
+})
 
-type="text/javascript"
-const months = ['January','February','March','April','May','June','July',
-'August','September','October','November','December'];       
-let tomorrow = new Date();
-tomorrow.setTime(tomorrow.getTime() + (1000*3600*24));       
-document.getElementById("spanDate").innerHTML = months[tomorrow.getMonth()] + " " + tomorrow.getDate()+ ", " + tomorrow.getFullYear(), + " " + tomorrow.getTime();
- 
+const countDownDate = new Date("May 1, 2023, 13:00:00 ").getTime();
+
+let updateEverySecond = setInterval(function() {
+    let now = new Date().getTime();
+
+    let distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("timer").innerHTML = days + "d " + hours + "t "
+    + minutes + "m " + seconds + "s ";
+})
+    
